9f0145836e51de3bf79ecd301b2a4a5c89531ed9
GonzaRamat <ramat.gonzalo@gmail.com>
2024-07-09 19:15:25 -0300

    nueva entrega

--- clase 1/main.js
+++ clase 1/main.js
@@ -1,5 +1,5 @@
 //Entrada de Datos
-let nombreCliente = prompt ("Ingrese su nombre y apellido: (ESC para salir)")
+/*let nombreCliente = prompt ("Ingrese su nombre y apellido: (ESC para salir)")
 
 while (nombreCliente != "ESC") {
 
@@ -73,18 +73,18 @@ else if (tipoCliente == 2) {
 else if (tipoCliente == 3) {
     TEA = 127.42 / 12
         }
-else {
+ else {
     TEA = 166.31 / 12
             }
-return (cuotaPura + TEA) /100;
+            return (cuotaPura + TEA) /100;
 
     }
 
 function informarPrestamo (nombreCliente, monto, plazo, cuotaPagar) {
+   
+    let textoPlazo 
 
-    let textoPlazo;
-
-    if (plazo == 1) {textoPlazo = "12";
+    if (plazo == 1) {textoPlazo ="12";
 
     } else if (plazo ==2) {textoPlazo = "24";
 
@@ -93,9 +93,77 @@ function informarPrestamo (nombreCliente, monto, plazo, cuotaPagar) {
     }
 
     alert ("Cliente: " + nombreCliente + "\n" + "Monto: " + monto + "\nPlazo: " + plazo + " meses\nCuota a pagar: "+ cuotaPagar.toFixed (2) + "pesos")
+}*/
+
+
+const bebidas =[
+    {id:1, nombre:"Zapatillas", marca: "adidas", precio: 180000 },
+    {id:2, nombre:"Zapatos", marca: "Lady Stork", precio: 165000 },
+    {id:3, nombre:"Ojotas", marca: "Havaianas", precio: 150000 },
+    {id:4, nombre:"Sandalias", marca: "Lady Stork", precio: 95000 },
+    {id:5, nombre:"Botas", marca: "Gola", precio: 150000 },
+    {id:6, nombre:"Borcegos", marca: "Grimoldi", precio: 100500 },
+    
+]
+ //console.log(bebidas.[0]);
+
+class productos{
+    constructor(productos){
+        this.lista = productos;
+    }
+
+    agregarProducto(nombreP, marcaP, precioP){
+        const producto = {id: this.generarId(), nombre: nombreP, marca:marcaP, precio: precioP }
+        this.lista.push (producto)
+        console.log("Producto Agregado");
+    }
+
+    buscarProducto (id) {
+        return this.lista.find (items => items.id == id);
+    }
+    
+    obtenerProducto (){
+        return this.lista;
+    }
+
+    totalProductos (){
+        return this.lista.length;
+    }
+
+   
+    generarId () {
+        let max = 0;
+        this.lista.forEach(item => {
+            if(item.id > max){
+                max = item.id;
+            }
+        return max + 1;
+
+        });
+
+    }
+
+    eliminarProducto (id) {
+        this.lista = this.lista.filter (items => items.id != id);
+        console.log("Se elimino el producto " + id);
+    }
 }
 
 
+const catalogo = new productos (bebidas);
+console.log(catalogo.obtenerProducto());
 
- 
- 
+let nombreProducto = prompt ("Ingrese el nombre de un nuevo Producto: ");
+let marcaProducto = prompt ("Ingrese la marca del producto")
+let precioProducto = parseFloat(prompt ("ingrese el precio del producto"));
+
+
+catalogo.agregarProducto (nombreProducto, marcaProducto, precioProducto);
+alert("Se agrego al catalogo el producto: " + nombreProducto + "Se agregó al catálogo la marca" + marcaProducto +  "\n Precio: $" + precioProducto);
+console.log(catalogo.obtenerProducto());
+console.log(catalogo.eliminarProducto(3));
+
+console.log(catalogo.obtenerProducto());
+
+
+alert("El total de tus productos en catalogo son: " + catalogo.totalProductos())
